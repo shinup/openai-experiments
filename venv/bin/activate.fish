27039f0f51b1624ec740e64458ed15a27e55332d
Shinu Pushpan <shinupushpan@Shinus-MacBook-Pro.local>
2024-10-12 09:59:05 -0700

    More chages

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shinupushpan/development/resume_editor/venv"
+set -gx VIRTUAL_ENV "/Users/shinupushpan/development/openai-experiments/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
